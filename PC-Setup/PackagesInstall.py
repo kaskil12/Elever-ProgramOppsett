03c69cd6ab05bbde31c1d3992431ea504f4142fc
Kasper Kilde <kasperkilde2@gmail.com>
2024-09-27 13:20:48 +0200

    fully functional tabs finally

--- PC-Setup/PackagesInstall.py
+++ PC-Setup/PackagesInstall.py
@@ -2,13 +2,13 @@ import os
 import tkinter as tk
 from tkinter import ttk
 
-# Initialize window
-window = tk.Tk()
-window.title("Installer")
-
 # Tab control variables
 ProgramsInstaller = True
 Fixes = False
+
+#root window
+root = tk.Tk()
+root.title("PC Setup")
 
 # Programs Installer tab variables
 DriveLetter = tk.StringVar(value="D:")
@@ -41,36 +41,33 @@ EjectDrive = False
 EjectDriveTK = tk.IntVar()
 
 # Tab switch function
-def switch():
-    global Fixes, ProgramsInstaller
-    ProgramsInstaller = not ProgramsInstaller
-    Fixes = not Fixes
 
 # Fixes Tab
-if Fixes:
+def FixesTab(tab):
     def FixButtons():
-        switchTabs = tk.Button(window, text="Switch Tabs", command=switch)
-        switchTabs.grid(row=0, column=0, padx=10, pady=10)
-        ADDRemove = tk.Button(window, text="Add/Remove Programs", command=AddRemove)
-        ADDRemove.grid(row=0, column=1, padx=10, pady=10)
-        SfcScan = tk.Button(window, text="SFC Scan", command=SfcScan)
-        SfcScan.grid(row=1, column=0, padx=10, pady=10)
-        window.mainloop()
-    
+        ADDRemove = tk.Button(tab, text="Add/Remove Programs", command=AddRemove)
+        ADDRemove.grid(row=0, column=0, padx=0, pady=10, sticky='w')
+        SfcScanButton = tk.Button(tab, text="SFC Scan", command=SfcScan)
+        SfcScanButton.grid(row=1, column=0, padx=0, pady=10, sticky='w')
+
     def AddRemove():
         print("Opening Add/Remove Programs")
+        # Implement the actual logic here
 
     def SfcScan():
         print("Running SFC Scan")
         os.system("sfc /scannow")
 
+    FixButtons()
 # Programs Installer tab
-if ProgramsInstaller:
-    def check_installed_packages():
+def ProgramsInstallerTab(tab):
+    label = tk.Label(tab, text="Programs Installer")
+    label.grid(padx=20, pady=20)
+    def check_installed_gridages(tab):
         global isOfficeInstalled, isTeamsInstalled, isOrdnettInstalled, isVsCodeInstalled
         global isChromeInstalled, isFirefoxInstalled, isPythonInstalled, isGeoGebraInstalled
 
-        print("Checking installed packages")
+        print("Checking installed gridages")
         isOfficeInstalled = os.path.exists(os.path.join(os.path.expanduser('~'), 'AppData', 'Local', 'Microsoft', 'Office'))
         isTeamsInstalled = os.path.exists(os.path.join(os.path.expanduser('~'), 'AppData', 'Local', 'Programs', 'Microsoft VS Code'))
         isOrdnettInstalled = os.path.exists(r"C:/Program Files (x86)/Kunnskapsforlaget/Ordnett Pluss")
@@ -80,76 +77,73 @@ if ProgramsInstaller:
         isPythonInstalled = os.path.exists('C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Python 3.12')
         isGeoGebraInstalled = os.path.exists(os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'GeoGebra'))
 
-    def update_progress(total, current):
+    def update_progress(total, current, tab):
         percent = (current / total) * 100
         progress_bar['value'] = percent
         progress_label.config(text=f"Progress: {int(percent)}%")
-        window.update_idletasks()
+        tab.update_idletasks()
 
-    def add_check_and_trash(row, label_text, install_var, is_installed_var, uninstall_command):
-        check = tk.Checkbutton(window, text=label_text, variable=install_var, image=uncheckedBox, indicatoron=False, selectimage=checkedBox, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat", selectcolor=window.cget("bg"))
+    def add_check_and_trash(row, label_text, install_var, is_installed_var, uninstall_command, tab):
+        check = tk.Checkbutton(tab, text=label_text, variable=install_var, image=uncheckedBox, indicatoron=False, selectimage=checkedBox, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat", selectcolor="white")
+
+
         check.grid(row=row, column=0, padx=10, pady=5, sticky='w')
 
         if is_installed_var:
-            trash_button = tk.Button(window, image=trashIcon, command=lambda: uninstall_packages(uninstall_command))
+            trash_button = tk.Button(tab, image=trashIcon, command=lambda: uninstall_gridages(uninstall_command, tab))
             trash_button.grid(row=row, column=1, padx=10, pady=5, sticky='e')
 
-    def checkBoxes():
+    def checkBoxes(tab):
         global progress_bar, progress_label
 
-        check_installed_packages()
+        check_installed_gridages(tab)
 
-        switchTabs = tk.Button(window, text="Switch Tabs", command=switch)
-        switchTabs.grid(row=0, column=1, padx=10, pady=10)
-
-        infotext = tk.Label(window, text="Choose the drive letter of the USB drive")
+        infotext = tk.Label(tab, text="Choose the drive letter of the USB drive")
         infotext.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky='w')
 
-        ejectCheckBox = tk.Checkbutton(window, text="Eject USB Drive", variable=EjectDrive, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat", selectcolor=window.cget("bg"))
+        ejectCheckBox = tk.Checkbutton(tab, text="Eject USB Drive", variable=EjectDrive, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat",selectcolor="white")
         ejectCheckBox.grid(row=1, column=1, padx=10, pady=5)
 
-        drive_entry = tk.Entry(window, textvariable=DriveLetter)
+        drive_entry = tk.Entry(tab, textvariable=DriveLetter)
         drive_entry.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky='w')
 
         row_num = 2
 
         # Add checkboxes with trash buttons
-        add_check_and_trash(row_num, "Office", installOfficeTK, isOfficeInstalled, "Office")
+        add_check_and_trash(row_num, "Office", installOfficeTK, isOfficeInstalled, "Office", tab)
         row_num += 1
-        add_check_and_trash(row_num, "Teams", installTeamsTK, isTeamsInstalled, "Teams")
+        add_check_and_trash(row_num, "Teams", installTeamsTK, isTeamsInstalled, "Teams", tab)
         row_num += 1
-        add_check_and_trash(row_num, "Ordnett", installOrdnettTK, isOrdnettInstalled, "Ordnett")
+        add_check_and_trash(row_num, "Ordnett", installOrdnettTK, isOrdnettInstalled, "Ordnett", tab)
         row_num += 1
-        add_check_and_trash(row_num, "Visual Studio Code", installVsCodeTK, isVsCodeInstalled, "VsCode")
+        add_check_and_trash(row_num, "Visual Studio Code", installVsCodeTK, isVsCodeInstalled, "VsCode", tab)
         row_num += 1
-        add_check_and_trash(row_num, "Google Chrome", installChromeTK, isChromeInstalled, "Chrome")
+        add_check_and_trash(row_num, "Google Chrome", installChromeTK, isChromeInstalled, "Chrome", tab)
         row_num += 1
-        add_check_and_trash(row_num, "Mozilla Firefox", installFirefoxTK, isFirefoxInstalled, "Firefox")
+        add_check_and_trash(row_num, "Mozilla Firefox", installFirefoxTK, isFirefoxInstalled, "Firefox", tab)
         row_num += 1
-        add_check_and_trash(row_num, "Python", installPythonTK, isPythonInstalled, "Python")
+        add_check_and_trash(row_num, "Python", installPythonTK, isPythonInstalled, "Python", tab)
         row_num += 1
-        add_check_and_trash(row_num, "GeoGebra", installGeoGebraTK, isGeoGebraInstalled, "GeoGebra")
+        add_check_and_trash(row_num, "GeoGebra", installGeoGebraTK, isGeoGebraInstalled, "GeoGebra", tab)
 
         # Web shortcuts
-        WebShortcutCheck = tk.Checkbutton(window, text="Nettside snarveier", variable=createWebShortcutTK, image=uncheckedBox, indicatoron=False, selectimage=checkedBox, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat", selectcolor=window.cget("bg"))
+        WebShortcutCheck = tk.Checkbutton(tab, text="Nettside snarveier", variable=createWebShortcutTK, image=uncheckedBox, indicatoron=False, selectimage=checkedBox, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat", selectcolor=tab.tk.eval("ttk::style lookup TFrame -background"))
         WebShortcutCheck.grid(row=row_num, column=0, padx=10, pady=5, sticky='w')
         row_num += 1
-        WebShortcutCheckKI = tk.Checkbutton(window, text="Nettside snarveier Karriere", variable=createWebShortcutTKKI, image=uncheckedBox, indicatoron=False, selectimage=checkedBox, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat", selectcolor=window.cget("bg"))
+        WebShortcutCheckKI = tk.Checkbutton(tab, text="Nettside snarveier Karriere", variable=createWebShortcutTKKI, image=uncheckedBox, indicatoron=False, selectimage=checkedBox, compound="left", padx=10, borderwidth=0, highlightthickness=0, relief="flat", selectcolor=tab.tk.eval("ttk::style lookup TFrame -background"))
         WebShortcutCheckKI.grid(row=row_num, column=0, padx=10, pady=5, sticky='w')
 
         # Progress bar and installation button
-        progress_bar = ttk.Progressbar(window, orient='horizontal', length=300, mode='determinate')
+        progress_bar = ttk.Progressbar(tab, orient='horizontal', length=300, mode='determinate')
         progress_bar.grid(row=row_num+1, column=0, columnspan=2, padx=10, pady=10)
 
-        progress_label = tk.Label(window, text="Progress: 0%")
+        progress_label = tk.Label(tab, text="Progress: 0%")
         progress_label.grid(row=row_num+2, column=0, columnspan=2, padx=10, pady=5)
 
-        installButton = tk.Button(window, text="Install", command=install_packages)
+        installButton = tk.Button(tab, text="Install", command=install_gridages)
         installButton.grid(row=row_num+3, column=0, columnspan=2, padx=10, pady=10)
 
-        window.mainloop()
-
-    def uninstall_packages(program):
+    def uninstall_gridages(program, tab):
         current_drive_letter = DriveLetter.get()
         uninstall_commands = {
             "Office": "start /wait {current_drive_letter}/OfficeOffline/setup.exe /configure {current_drive_letter}/OfficeOffline/ElvisUninstall.xml",
@@ -166,7 +160,7 @@ if ProgramsInstaller:
         if uninstall_command:
             os.system(f"powershell.exe -ExecutionPolicy Bypass -Command {uninstall_command}")
 
-    def install_packages():
+    def install_gridages(tab):
         total_installations = 8
         current_installation = 0
         current_drive_letter = DriveLetter.get()
@@ -174,86 +168,86 @@ if ProgramsInstaller:
             print("Installing Office")
             os.system("start /wait .\OfficeOffline\setup.exe /configure .\OfficeOffline\Elvis.xml")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
         
         if installTeams:
             os.system(r"Start /wait setup.exe /configure installOffice.xml")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installTeamsTK.get():
             print("Installing Teams")
             os.system(rf'start /wait .\TeamsOffline\teamsbootstrapper.exe -p -o "{current_drive_letter}\TeamsOffline\MSTeams-x64.msix"')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
         
         if installOrdnett:
             os.system(r"msiexec /i Teams_windows_x64.msi")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installOrdnettTK.get():
             print("Installing Ordnett")
             os.system(rf'msiexec /i "{current_drive_letter}\OrdnettOffline\ordnettpluss-3.3.7-innlandet_fylkeskommune.msi" ALLUSERS=2 /qb')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
         
         if installVsCode:
             print("Installing Visual Studio Code")
             os.system(rf'start /wait .\VsCodeOffline\Code.exe')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
         
         if installChrome:
             os.system(r"msiexec /i Ordnett.exe")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installVsCodeTK.get():
             print("Installing VS Code")
             os.system(r"VSCodeUserSetup.exe /S")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installChromeTK.get():
             print("Installing Google Chrome")
             os.system(rf'start /wait .\ChromeOffline\ChromeStandaloneSetup64.exe')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
         
         if installFirefox:
             print("Installing Mozilla Firefox")
             os.system(r'start /wait .\FirefoxOffline\FireFoxInstall.exe /S')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installPython:
             os.system(r"ChromeSetup.exe /silent /install")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installFirefoxTK.get():
             print("Installing Firefox")
             os.system(r"FirefoxInstaller.exe /S")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installPythonTK.get():
             print("Installing Python")
             os.system(r'start /wait .\PythonOffline\python-3.12.6-amd64.exe /quiet PrependPath=1')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
             
         if installGeoGebra:
             os.system(r"python-3.12.0.exe /quiet InstallAllUsers=1 PrependPath=1")
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
 
         if installGeoGebraTK.get():
             print("Installing GeoGebra")
             os.system(rf'msiexec /i "{current_drive_letter}\GeogebraOffline\GeoGebra-Windows-Installer-6-0-848-0.msi" ALLUSERS=2 /qb')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
         if createWebShortcut:
             print("Creating Web Shortcut")
             os.system(r'''powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\ElverumVGS.url');$s.TargetPath='http://elverum.vgs.no';$s.Save()"''')
@@ -262,7 +256,7 @@ if ProgramsInstaller:
 
             os.system(r'''powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\VismaInSchool.url');$s.TargetPath='https://elverum-vgs.inschool.visma.no/Login.jsp';$s.Save()"''')
             current_installation += 1
-            update_progress(total_installations, current_installation)
+            update_progress(total_installations, current_installation, tab)
         if createWebShortcutKI:
             print("Creating Web Shortcut For KI")
             #creating karriere innlandet shortcut
@@ -277,4 +271,24 @@ if ProgramsInstaller:
             os.system(f"powershell -command \"$driveEject = New-Object -comObject Shell.Application; $driveEject.Namespace(17).ParseName('{current_drive_letter}').InvokeVerb('Eject')\"")
         
 
-    checkBoxes()
+    checkBoxes(tab)
+def main():
+    notebook = ttk.Notebook(root)
+
+
+    tab1 = ttk.Frame(notebook)
+    tab2 = ttk.Frame(notebook)
+
+    notebook.add(tab1, text="Programs Installer")
+    notebook.add(tab2, text="Fixes")
+
+
+    ProgramsInstallerTab(tab1)
+    FixesTab(tab2)
+
+    notebook.grid(padx=10, pady=10, sticky="nsew")
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.mainloop()
+if __name__ == "__main__":
+    main()
