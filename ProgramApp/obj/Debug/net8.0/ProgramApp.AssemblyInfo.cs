851f1c501b112c16bc4f59a85aa48d19c334f798
Kasper Kilde <kasperkilde2@gmail.com>
2024-11-17 19:25:55 +0100

    made the program ask for admin rights

--- ProgramApp/obj/Debug/net8.0/ProgramApp.AssemblyInfo.cs
+++ ProgramApp/obj/Debug/net8.0/ProgramApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918b05cab8e0bac1b66638a1ffc03f9d53b93ecb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097463e4d0dfb30ceada3968886a1c1fb96b109c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
