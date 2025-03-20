a3e2f42d4abc81dc1ceb72675dfd44688b13997a
Kasper Kilde <kasperkilde2@gmail.com>
2025-03-20 11:44:06 +0100

    Icon

--- ProgramApp/obj/Debug/net8.0/ProgramApp.AssemblyInfo.cs
+++ ProgramApp/obj/Debug/net8.0/ProgramApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1450138c9b07cf6868e1b405893c86f1a89d1e7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c070343f6f6fa808f3a2612deb223e51846eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
