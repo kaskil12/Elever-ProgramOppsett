f3ea4cef00679058c65d8caf23af092c8733da92
Kasper Kilde <kasperkilde2@gmail.com>
2025-03-25 14:31:54 +0100

    Update
    Design improvements

--- ProgramApp/obj/Debug/net8.0/ProgramApp.AssemblyInfo.cs
+++ ProgramApp/obj/Debug/net8.0/ProgramApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8da531078977b11b0e26a23028fb37f38981d61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b20903ff03e53965690a54caf3bb0f6c621c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
