a91f7109972407b428dfecbcfe27aeb7485a7035
Kasper Kilde <kasperkilde2@gmail.com>
2025-03-28 12:04:13 +0100

    Build

--- ProgramApp/obj/Release/net8.0/win-x64/ProgramApp.AssemblyInfo.cs
+++ ProgramApp/obj/Release/net8.0/win-x64/ProgramApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8518b6418a6450fff4e570d79db12178567d8117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea0715c2ed150de799c6d34399c76e73737a0c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgramApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
